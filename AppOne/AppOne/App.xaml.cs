bbb0ddeca0a7829c62dfa0045d6ad2918a9b4731
AnandSivadas <AnandSivadas@ANANDSIVADAS-LA>
2022-03-21 18:47:21 +0530

    Add project files.

---
+++ AppOne/AppOne/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AppOne.Services;
+using AppOne.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppOne
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppOne/AppOne/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppOne.Models;
+using AppOne.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppOne.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
